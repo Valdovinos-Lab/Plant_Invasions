8e79a9604a93baf92bdee69b95d8811fb61098dc
Sabine Dritz <70649535+sjdritz@users.noreply.github.com>
2023-01-13 13:39:07 -0800

    Update integrate.m
    storing interaction data in metadata

--- private/integrate.m
+++ private/integrate.m
@@ -1,9 +1,9 @@
-function [final_parameters] = integrate(data, file_name, initial_state, tspan)
+function [final_parameters] = integrate(file_name, initial_state, tspan)
 
     global J_pattern simulation extinct_level_p network_metadata 
 
     % determine dimensions of dataset
-    [row, col] = size(data);
+    [row, col] = size(network_metadata.data);
 
     % integrate the dynamic model
     options = odeset('JPattern', J_pattern, 'NonNegative', 1:(2 * row) + col);
